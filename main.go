d9a6f1f70411764179fc39b01c905b89e08af101
Bobby Powers <bobbypowers@gmail.com>
2012-12-27 09:51:31 -0500

    listing of all active pids

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"runtime"
+	"strconv"
+)
+
+func isDigit(d uint8) bool {
+	return d >= '0' && d <= '9'
+}
+
+func pidList() ([]int, error) {
+	procLs, err := ioutil.ReadDir("/proc")
+	if err != nil {
+		return nil, fmt.Errorf("ReadDir(/proc): %s", err)
+	}
+
+	pids := make([]int, 0, len(procLs))
+	for _, pInfo := range procLs {
+		if !isDigit(pInfo.Name()[0]) || !pInfo.IsDir() {
+			continue
+		}
+		pidInt, err := strconv.Atoi(pInfo.Name())
+		if err != nil {
+			return nil, fmt.Errorf("Atoi(%s): %s", pInfo.Name(), err)
+		}
+		pids = append(pids, pidInt)
+	}
+	return pids, nil
+}
+
+func main() {
+	// give us as much parallelism as possible
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	pids, err := pidList()
+	if err != nil {
+		log.Printf("pidList: %s", err)
+		return
+	}
+	log.Printf("pids: %v", pids)
+}
