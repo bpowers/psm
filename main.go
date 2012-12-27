db71101fa089123d95d950bb667c5e4116428159
Bobby Powers <bobbypowers@gmail.com>
2012-12-27 15:12:51 -0500

    implement procName

--- main.go
+++ main.go
@@ -5,14 +5,31 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"path"
 	"runtime"
 	"strconv"
+	"strings"
+	"sync"
 )
 
+// store info about a command (group of processes), similar to how
+// ps_mem works.
+type CmdMemInfo struct {
+	PIDs    []int
+	Name    string
+	Pss     int
+	Shared  int
+	Swapped int
+}
+
+// isDigit returns true if the rune d represents an ascii digit
+// between 0 and 9, inclusive.
 func isDigit(d uint8) bool {
 	return d >= '0' && d <= '9'
 }
 
+// pidList returns a list of the process-IDs of every currently
+// running process on the local system.
 func pidList() ([]int, error) {
 	procLs, err := ioutil.ReadDir("/proc")
 	if err != nil {
@@ -33,9 +50,62 @@ func pidList() ([]int, error) {
 	return pids, nil
 }
 
+// procName gets the process name for a worker.  It first checks the
+// value of /proc/$PID/cmdline.  If setproctitle(3) has been called,
+// it will use this.  Otherwise it uses the value of
+// path.Base(/proc/$PID/exe), which has info on whether the executable
+// has changed since the process was exec'ed.
+func procName(pid int) (string, error) {
+	p, err := os.Readlink(fmt.Sprintf("/proc/%d/exe", pid))
+	// this would return an error if the PID doesn't
+	// exist, or if the PID refers to a kernel thread.
+	if err != nil {
+		return "", nil
+	}
+	// cmdline is the null separated list of command line
+	// arguments for the process, unless setproctitle(3)
+	// has been called, in which case it is the
+	argsB, err := ioutil.ReadFile(fmt.Sprintf("/proc/%d/cmdline", pid))
+	if err != nil {
+		return "", fmt.Errorf("ReadFile(%s): %s", fmt.Sprintf("/proc/%d/cmdline", pid), err)
+	}
+	args := strings.Split(string(argsB), "\000")
+	n := args[0]
+
+	exe := path.Base(p)
+	if strings.HasPrefix(exe, n) {
+		n = exe
+	}
+	return n, nil
+}
+
+func worker(pidRequest chan int, wg *sync.WaitGroup, result chan *CmdMemInfo) {
+	for pid := range pidRequest {
+		var err error
+		cmi := new(CmdMemInfo)
+
+		cmi.PIDs = []int{pid}
+		cmi.Name, err = procName(pid)
+		if err != nil {
+			log.Printf("procName(%d): %s", pid, err)
+			wg.Done()
+			continue
+		} else if cmi.Name == "" {
+			// XXX: This happens with kernel
+			// threads. maybe warn? idk.
+			wg.Done()
+			continue
+		}
+
+		log.Printf("%#v", cmi)
+		wg.Done()
+	}
+}
+
 func main() {
+	nCPU := runtime.NumCPU()
 	// give us as much parallelism as possible
-	runtime.GOMAXPROCS(runtime.NumCPU())
+	runtime.GOMAXPROCS(nCPU)
 
 	if os.Geteuid() != 0 {
 		fmt.Printf("FATAL: root required.")
@@ -47,5 +117,20 @@ func main() {
 		log.Printf("pidList: %s", err)
 		return
 	}
+
+	var wg sync.WaitGroup
+	work := make(chan int, len(pids))
+	result := make(chan *CmdMemInfo, len(pids))
+
+	for i := 0; i < nCPU; i++ {
+		go worker(work, &wg, result)
+	}
+
+	wg.Add(len(pids))
+	for _, pid := range pids {
+		work <- pid
+	}
+	wg.Wait()
+
 	log.Printf("pids: %v", pids)
 }
